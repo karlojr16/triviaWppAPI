801009eca11dc56658e744018f4597c773ca1e95
Givan08 <Givan.ram01@gmail.com>
2026-02-11 15:31:36 -0600

    Diseño de botones de opciones, asi como para que aparezcan al azar, y muestre las preguntas concecuentes hasta la pantalla de puntaje final

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
 
 var datos = {};
+var indicePregunta = 0;
+var puntaje = 0
 
 function extraer(){
-    fetch ("https://opentdb.com/api.php?amount=20&category=15&type=multiple" )
+    fetch ("https://opentdb.com/api.php?amount=10&category=15&type=multiple" )
     .then(response => response.json())
-    .then(data =>{datos = data; Mostrar();});
+    .then(data =>{
+        datos = data; 
+        indicePregunta = 0;
+        puntaje = 0;
+        Mostrar();});
 }
 
 function Mostrar(){
     var tarjeta = document.getElementById("texto-pregunta");
     var cajaOpciones = document.getElementById("contenedor-opciones");
-
+    var contadorTexto = document.getElementById("contador")
     cajaOpciones.innerHTML = "";
-
-    var preguntaActual = datos.results[0]
-
+    
+    var preguntaActual = datos.results[indicePregunta]
     tarjeta.innerHTML = preguntaActual.question;
 
-    var btnCorrecto = document.createElement("button");
-    btnCorrecto.innerHTML = preguntaActual.correct_answer;
-    cajaOpciones.appendChild(btnCorrecto);
+    contadorTexto.innerText = `Pregunta ${indicePregunta + 1} de ${datos.results.length}`;
 
-    respuestaI(0, cajaOpciones);
+    var todasLasOpciones = [...preguntaActual.incorrect_answers, preguntaActual.correct_answer];
+    todasLasOpciones.sort(() => Math.random() - 0.5);
+
+    todasLasOpciones.forEach(opcion => {
+        var btn = document.createElement("button");
+        btn.innerHTML = opcion;
+        btn.onclick = () => validarRespuesta(opcion, preguntaActual.correct_answer);
+        cajaOpciones.appendChild(btn);
+    });
+}
+function validarRespuesta(seleccionada, correcta) {
+    if (seleccionada === correcta) {
+        puntaje++;
+    }
+    indicePregunta++;
+    if (indicePregunta < datos.results.length) {
+        Mostrar(); 
+    } else {
+        mostrarResultadoFinal(); 
+    }
+}
+function mostrarResultadoFinal() {
+    document.getElementById("contenedor-juego").classList.add("oculto");
+    document.getElementById("pantalla-resultado").classList.remove("oculto");
+    
+    document.getElementById("puntaje-final").innerText = `Puntaje: ${puntaje} / ${datos.results.length}`;
 }
 function respuestaI(pos, tarjeta){
     for(let i = 0; i < 3; i++){
